00c50329ad21908e93b628a31d928762c9662437
Mathias Remshardt <remshams@gmail.com>
2024-01-21 11:32:03 +0100

    feat(jira): adds charm logger

--- jira/main.go
+++ jira/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/charmbracelet/log"
 	"github.com/remshams/common/logger"
 )
 
 func main() {
 	logger.PrepareLogger()
+	log.Debug("Hello, World!")
 	fmt.Println("Hello, World!")
 }
