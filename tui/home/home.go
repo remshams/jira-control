9193e5c2786d2417a270fb067ac3dd724b1f37fc
Mathias Remshardt <remshams@gmail.com>
2024-01-23 19:33:46 +0100

    feat(tui-home): adds rendering help

--- tui/home/home.go
+++ tui/home/home.go
@@ -6,16 +6,21 @@ import (
 	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+	"github.com/remshams/common/tui/bubbles/help"
 	"github.com/remshams/common/tui/bubbles/toast"
 	"github.com/remshams/common/tui/styles"
 )
 
 type Model struct {
 	toast toast.Model
+	help  help.Model
 }
 
 func New() Model {
-	return Model{}
+	return Model{
+		toast: toast.New(),
+		help:  help.New(),
+	}
 }
 
 func (m Model) Init() tea.Cmd {
@@ -36,10 +41,19 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m Model) View() string {
-	return fmt.Sprintf("Jira home\n%s", m.renderToast())
+	return fmt.Sprintf(
+		"Jira home\n%s\n%s",
+		m.renderHelp(),
+		m.renderToast(),
+	)
 }
 
 func (m Model) renderToast() string {
 	style := lipgloss.NewStyle().PaddingTop(styles.Padding)
 	return style.Render(m.toast.View())
+}
+
+func (m Model) renderHelp() string {
+	style := lipgloss.NewStyle().PaddingTop(styles.Padding)
+	return style.Render(m.help.View())
 }
