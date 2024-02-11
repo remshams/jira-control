65ffe848faee56465ff298fea787ae3acd89f367
Mathias Remshardt <remshams@gmail.com>
2024-02-11 10:58:39 +0100

    feat(tui-Issue): adds rendering title and keymap

--- tui/issue/issue.go
+++ tui/issue/issue.go
@@ -1,6 +1,34 @@
 package issue
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	"github.com/charmbracelet/bubbles/key"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/remshams/common/tui/bubbles/help"
+	title "github.com/remshams/common/tui/bubbles/page_title"
+	"github.com/remshams/common/tui/bubbles/tabs"
+	common "github.com/remshams/jira-control/tui/_common"
+)
+
+type keyMap struct {
+	global common.GlobalKeyMap
+	tab    tabs.TabKeyMap
+}
+
+func (k keyMap) ShortHelp() []key.Binding {
+	return []key.Binding{
+		k.tab.Tab,
+		k.global.Quit,
+	}
+}
+
+func (k keyMap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{}
+}
+
+var issueKeys = keyMap{
+	global: common.GlobalKeys,
+	tab:    tabs.TabKeys,
+}
 
 type Model struct {
 }
@@ -10,7 +38,10 @@ func New() Model {
 }
 
 func (m Model) Init() tea.Cmd {
-	return nil
+	return tea.Batch(
+		title.CreateSetPageTitleMsg("Issue"),
+		help.CreateSetKeyMapMsg(issueKeys),
+	)
 }
 
 func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
