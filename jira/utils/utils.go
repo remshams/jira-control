9a66a3e975abd5376e44d6cdd98418688e6b8564
Mathias Remshardt <remshams@gmail.com>
2024-02-25 12:03:44 +0100

    feat(jira-utils): adds utility function to parse jira string to time

---
+++ jira/utils/utils.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/charmbracelet/log"
+)
+
+func JiraDateToTime(timeString string) (time.Time, error) {
+	t, err := time.Parse(
+		"2006-01-02T15:04:05.999-0700",
+		timeString,
+	)
+	if err != nil {
+		log.Errorf("JiraDateToTime: Could not parse time: %v", err)
+		return time.Time{}, err
+	}
+	return t, nil
+}
---
+++ jira/utils/utils_test.go
@@ -0,0 +1,42 @@
+package utils
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestJiraDateToTime_Valid(t *testing.T) {
+	year := 2024
+	month := time.February
+	day := 19
+	hour := 19
+	minute := 3
+	second := 50
+	nanosecond := 0
+	location, _ := time.LoadLocation(time.Local.String())
+	expected := time.Date(year, month, day, hour, minute, second, nanosecond, location)
+	convertedTime, err := JiraDateToTime(
+		fmt.Sprintf(
+			"%04d-%02d-%02dT%02d:%02d:%02d.%03d+0100",
+			year,
+			month,
+			day,
+			hour,
+			minute,
+			second,
+			nanosecond,
+		),
+	)
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, convertedTime)
+}
+
+func TestJiraDateToTime_Invalid(t *testing.T) {
+	_, err := JiraDateToTime("2024-02-1920:03:50.00+0100")
+
+	assert.Error(t, err)
+}
