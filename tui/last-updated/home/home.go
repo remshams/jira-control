6f55f60175bc8a952e0a585f44dddb9c5f0b33db
Mathias Remshardt <remshams@gmail.com>
2024-03-10 19:41:09 +0100

    feat(tui-LastUpdated): adds view states

--- tui/last-updated/home/home.go
+++ tui/last-updated/home/home.go
@@ -5,6 +5,11 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/remshams/common/tui/bubbles/help"
 	title "github.com/remshams/common/tui/bubbles/page_title"
+	"github.com/remshams/common/tui/bubbles/spinner"
+	"github.com/remshams/common/tui/bubbles/table"
+	"github.com/remshams/common/tui/styles"
+	"github.com/remshams/common/tui/utils"
+	jira "github.com/remshams/jira-control/jira/public"
 	common "github.com/remshams/jira-control/tui/_common"
 	tui_jira "github.com/remshams/jira-control/tui/jira"
 )
@@ -27,25 +32,84 @@ var LastUpdatedKeys = LastUpdatedKeymap{
 	global: common.GlobalKeys,
 }
 
+const (
+	lastUpdatedListStateLoading utils.ViewState = "lastUpdatedListStateLoading"
+	lastUpdatedListStateLoaded  utils.ViewState = "lastUpdatedListStateLoaded"
+)
+
 type Model struct {
 	adapter tui_jira.JiraAdapter
+	table   table.Model[[]jira.Issue]
+	spinner spinner.Model
+	state   utils.ViewState
 }
 
 func New(adapter tui_jira.JiraAdapter) Model {
 	return Model{
 		adapter: adapter,
+		table: table.
+			New(createTableColumns, createTableRows, 5, 10).
+			WithNotDataMessage("No issues"),
+		spinner: spinner.New().WithLabel("Loading issues"),
+		state:   lastUpdatedListStateLoading,
 	}
 }
 
 func (m Model) Init() tea.Cmd {
-	return tea.Batch(title.CreateSetPageTitleMsg("Last Updated Issues"), help.CreateSetKeyMapMsg(LastUpdatedKeys))
+	return tea.Batch(title.CreateSetPageTitleMsg("Last Updated Issues"), help.CreateSetKeyMapMsg(LastUpdatedKeys), m.spinner.Tick())
 }
 
 func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
 	var cmd tea.Cmd
+	switch m.state {
+	case lastUpdatedListStateLoading:
+		cmd = m.processLoadingUpdate(msg)
+	case lastUpdatedListStateLoaded:
+		cmd = m.proccessLoadedUpdate(msg)
+	}
 	return m, cmd
 }
 
+func (m *Model) processLoadingUpdate(msg tea.Msg) tea.Cmd {
+	var cmd tea.Cmd
+	m.spinner, cmd = m.spinner.Update(msg)
+	return cmd
+}
+
+func (m *Model) proccessLoadedUpdate(msg tea.Msg) tea.Cmd {
+	var cmd tea.Cmd
+	m.table, cmd = m.table.Update(msg)
+	return cmd
+}
+
 func (m Model) View() string {
-	return "Last Updated Issues View"
+	switch m.state {
+	case lastUpdatedListStateLoading:
+		return m.spinner.View()
+	case lastUpdatedListStateLoaded:
+		return m.table.View()
+	default:
+		return ""
+	}
+}
+
+func createTableColumns(tableWidth int) []table.Column {
+	return []table.Column{
+		{Title: "Key", Width: styles.CalculateDimensionsFromPercentage(20, tableWidth, 20)},
+		{Title: "Updated", Width: styles.CalculateDimensionsFromPercentage(20, tableWidth, 20)},
+		{Title: "Summary", Width: styles.CalculateDimensionsFromPercentage(60, tableWidth, styles.UnlimitedDimension)},
+	}
+}
+
+func createTableRows(issues []jira.Issue) []table.Row {
+	rows := []table.Row{}
+
+	for _, issue := range issues {
+		rows = append(rows, table.Row{
+			issue.Key,
+			issue.Project.Updated.Format("2006-01-02 15:04:05"),
+			issue.Summary,
+		})
+	}
+	return rows
 }
