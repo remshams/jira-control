5702fdd92f39a913eeb3257d9df3bf80fcd57760
Mathias Remshardt <remshams@gmail.com>
2024-03-10 16:05:33 +0100

    feat(tui-LastUpdated): adds global keymap

--- tui/last-updated/home/home.go
+++ tui/last-updated/home/home.go
@@ -1,10 +1,31 @@
 package tui_last_updated
 
 import (
+	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/remshams/common/tui/bubbles/help"
 	title "github.com/remshams/common/tui/bubbles/page_title"
+	common "github.com/remshams/jira-control/tui/_common"
 	tui_jira "github.com/remshams/jira-control/tui/jira"
 )
+
+type LastUpdatedKeymap struct {
+	global common.GlobalKeyMap
+}
+
+func (m LastUpdatedKeymap) ShortHelp() []key.Binding {
+	return m.global.KeyBindings()
+}
+
+func (m LastUpdatedKeymap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{
+		m.ShortHelp(),
+	}
+}
+
+var LastUpdatedKeys = LastUpdatedKeymap{
+	global: common.GlobalKeys,
+}
 
 type Model struct {
 	adapter tui_jira.JiraAdapter
@@ -17,7 +38,7 @@ func New(adapter tui_jira.JiraAdapter) Model {
 }
 
 func (m Model) Init() tea.Cmd {
-	return title.CreateSetPageTitleMsg("Last Updated Issues")
+	return tea.Batch(title.CreateSetPageTitleMsg("Last Updated Issues"), help.CreateSetKeyMapMsg(LastUpdatedKeys))
 }
 
 func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
