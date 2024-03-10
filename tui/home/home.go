eba58878d530c9f813e02583bb1b9e6641114506
Mathias Remshardt <remshams@gmail.com>
2024-03-10 15:59:28 +0100

    feat(tui-Home): adds last updated tab

--- tui/home/home.go
+++ tui/home/home.go
@@ -16,37 +16,41 @@ import (
 	issue_home "github.com/remshams/jira-control/tui/issue/home"
 	issue_search_result "github.com/remshams/jira-control/tui/issue/search/search-result"
 	tui_jira "github.com/remshams/jira-control/tui/jira"
+	tui_last_updated "github.com/remshams/jira-control/tui/last-updated/home"
 	app_store "github.com/remshams/jira-control/tui/store"
 	"github.com/remshams/jira-control/tui/worklog/details"
 )
 
 const (
-	stateIssue   utils.ViewState = "issue"
-	stateWorklog utils.ViewState = "worklog"
+	stateIssue       utils.ViewState = "issue"
+	stateWorklog     utils.ViewState = "worklog"
+	stateLastUpdated utils.ViewState = "last_updated"
 )
 
 type Model struct {
-	adapter tui_jira.JiraAdapter
-	tab     tabs.Model
-	title   title.Model
-	toast   toast.Model
-	help    help.Model
-	issue   issue_home.Model
-	worklog worklog_details.Model
-	state   utils.ViewState
+	adapter     tui_jira.JiraAdapter
+	tab         tabs.Model
+	title       title.Model
+	toast       toast.Model
+	help        help.Model
+	issue       issue_home.Model
+	worklog     worklog_details.Model
+	lastUpdated tui_last_updated.Model
+	state       utils.ViewState
 }
 
 func New(adapter tui_jira.JiraAdapter) Model {
 	return Model{
 		tab: tabs.New(
-			[]string{"Worklog", "Issues"},
+			[]string{"Worklog", "Issues", "Last Updated"},
 		),
-		title:   title.New(),
-		toast:   toast.New(),
-		help:    help.New(),
-		worklog: worklog_details.New(adapter),
-		issue:   issue_home.New(adapter),
-		state:   stateWorklog,
+		title:       title.New(),
+		toast:       toast.New(),
+		help:        help.New(),
+		worklog:     worklog_details.New(adapter),
+		issue:       issue_home.New(adapter),
+		lastUpdated: tui_last_updated.New(adapter),
+		state:       stateWorklog,
 	}
 }
 
@@ -93,6 +97,9 @@ func (m *Model) processTab(msg tabs.TabSelectedMsg) tea.Cmd {
 	case 1:
 		cmd = m.issue.Init()
 		m.state = stateIssue
+	case 2:
+		cmd = m.lastUpdated.Init()
+		m.state = stateLastUpdated
 	}
 	return cmd
 }
@@ -104,6 +111,8 @@ func (m *Model) processUpdate(msg tea.Msg) tea.Cmd {
 		cmd = m.processIssueUpdate(msg)
 	case stateWorklog:
 		m.worklog, cmd = m.worklog.Update(msg)
+	case stateLastUpdated:
+		m.lastUpdated, cmd = m.lastUpdated.Update(msg)
 	}
 	return cmd
 }
@@ -147,6 +156,8 @@ func (m Model) renderContent() string {
 		return style.Render(m.issue.View())
 	case stateWorklog:
 		return style.Render(m.worklog.View())
+	case stateLastUpdated:
+		return style.Render(m.lastUpdated.View())
 	default:
 		return "View does not exist"
 	}
