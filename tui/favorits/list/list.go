62c30c0c303312eb5c6413bea9593e1db5cffaf7
Mathias Remshardt <remshams@gmail.com>
2024-03-24 16:59:44 +0100

    feat(tui-Favorites): scaffolds favorite list

--- tui/favorits/list/list.go
+++ tui/favorits/list/list.go
@@ -1,33 +1,30 @@
-package favorite_home
+package favorite_list
 
 import (
 	tea "github.com/charmbracelet/bubbletea"
-	favorite_list "github.com/remshams/jira-control/tui/favorits/list"
+	title "github.com/remshams/common/tui/bubbles/page_title"
 	tui_jira "github.com/remshams/jira-control/tui/jira"
 )
 
 type Model struct {
 	adapter tui_jira.JiraAdapter
-	list    favorite_list.Model
 }
 
 func New(adapter tui_jira.JiraAdapter) Model {
 	return Model{
 		adapter: adapter,
-		list:    favorite_list.New(adapter),
 	}
 }
 
 func (m Model) Init() tea.Cmd {
-	return m.list.Init()
+	return title.CreateSetPageTitleMsg("Favorites")
 }
 
 func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
 	var cmd tea.Cmd
-	m.list, cmd = m.list.Update(msg)
 	return m, cmd
 }
 
 func (m Model) View() string {
-	return m.list.View()
+	return "Favorites"
 }
