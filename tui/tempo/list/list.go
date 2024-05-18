ca97781e2973f2f9a9ee182c033bbfed15b41d7c
Mathias Remshardt <remshams@gmail.com>
2024-05-18 18:42:51 +0200

    feat(tui-TempoWorklogList): adds table to view

--- tui/tempo/list/list.go
+++ tui/tempo/list/list.go
@@ -2,10 +2,14 @@ package tempo_workloglist
 
 import (
 	"fmt"
+	"strconv"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/log"
 	title "github.com/remshams/common/tui/bubbles/page_title"
 	"github.com/remshams/common/tui/bubbles/spinner"
+	"github.com/remshams/common/tui/bubbles/table"
+	"github.com/remshams/common/tui/styles"
 	"github.com/remshams/common/tui/utils"
 	jira "github.com/remshams/jira-control/jira/public"
 	tui_jira "github.com/remshams/jira-control/tui/jira"
@@ -56,15 +60,20 @@ type Model struct {
 	adapter  tui_jira.JiraAdapter
 	state    utils.ViewState
 	worklogs []jira.TempoWorklog
+	table    table.Model[[]jira.TempoWorklog]
 	spinner  spinner.Model
 }
 
 func New(adapter tui_jira.JiraAdapter) Model {
-	return Model{
+	model := Model{
 		adapter: adapter,
 		state:   tempoWorklogStateLoading,
 		spinner: spinner.New().WithLabel("Loading worklogs..."),
 	}
+	model.table = table.
+		New(createTableColumns, createTableRows, 5, 10).
+		WithNotDataMessage("No worklogs")
+	return model
 }
 
 func (m Model) Init() tea.Cmd {
@@ -95,6 +104,7 @@ func (m *Model) processLoadingUpdate(msg tea.Msg) tea.Cmd {
 	case LoadWorklogsSuccessAction:
 		m.state = tempoWorklogStateLoaded
 		m.worklogs = msg.Worklogs
+		cmd = table.CreateTableDataUpdatedAction(m.worklogs)
 	case LoadWorklogsErrorAction:
 		m.state = tempoWorklogStateError
 		m.worklogs = []jira.TempoWorklog{}
@@ -106,13 +116,14 @@ func (m *Model) processLoadingUpdate(msg tea.Msg) tea.Cmd {
 
 func (m *Model) processLoadedUpdate(msg tea.Msg) tea.Cmd {
 	var cmd tea.Cmd
+	m.table, cmd = m.table.Update(msg)
 	return cmd
 }
 
 func (m Model) View() string {
 	switch m.state {
 	case tempoWorklogStateLoaded:
-		return fmt.Sprintf("Worklogs: %d", len(m.worklogs))
+		return m.table.View()
 	case tempoWorklogStateLoading:
 		return m.spinner.View()
 	case tempoWorklogStateError:
@@ -120,5 +131,27 @@ func (m Model) View() string {
 	default:
 		return ""
 	}
+}
+func createTableColumns(tableWidth int) []table.Column {
+	return []table.Column{
+		{Title: "Id", Width: styles.CalculateDimensionsFromPercentage(20, tableWidth, 20)},
+		{Title: "Start", Width: styles.CalculateDimensionsFromPercentage(40, tableWidth, 20)},
+		{Title: "Time Spent", Width: styles.CalculateDimensionsFromPercentage(10, tableWidth, 10)},
+		{Title: "Description", Width: styles.CalculateDimensionsFromPercentage(30, tableWidth, styles.UnlimitedDimension)},
+	}
+}
 
+func createTableRows(worklogs []jira.TempoWorklog) []table.Row {
+	rows := []table.Row{}
+
+	log.Debugf("Worklogs: %v", len(worklogs))
+	for _, worklog := range worklogs {
+		rows = append(rows, table.Row{
+			strconv.Itoa(worklog.Id),
+			worklog.Start.Format("2006-01-02 15:04"),
+			fmt.Sprintf("%.1f h", float64(worklog.TimeSpentInSeconds)/3600),
+			"",
+		})
+	}
+	return rows
 }
