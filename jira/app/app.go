06e2f5ef037b947c9ee88e67389310bd8e65ad20
Mathias Remshardt <remshams@gmail.com>
2024-05-18 14:21:05 +0200

    feat(jira-app): adds an example request to retrieve worklogs
    Requests all worklogs of the current month

--- jira/app/app.go
+++ jira/app/app.go
@@ -10,16 +10,20 @@ import (
 	"github.com/remshams/jira-control/jira/favorite"
 	"github.com/remshams/jira-control/jira/issue"
 	issue_worklog "github.com/remshams/jira-control/jira/issue/worklog"
+	tempo_worklog "github.com/remshams/jira-control/jira/tempo/worklog"
 )
 
 type App struct {
 	Production          bool
 	Url                 url.URL
+	TempoUrl            url.URL
 	Username            string
 	ApiToken            string
+	TempoApiToken       string
 	IssueAdapter        issue.IssueAdapter
 	IssueWorklogAdapter issue_worklog.WorklogAdapter
 	FavoriteAdapter     favorite.FavoriteAdapter
+	TempoWorklogAdapter tempo_worklog.WorklistAdapter
 }
 
 func AppFromEnv() (*App, error) {
@@ -33,6 +37,11 @@ func AppFromEnv() (*App, error) {
 		log.Errorf("App: JIRA_URL not set or invalid: %v", err)
 		return nil, errors.New("JIRA_URL not set or invalid")
 	}
+	tempoUrl, err := url.Parse(os.Getenv("TEMPO_URL"))
+	if err != nil || url.String() == "" {
+		log.Errorf("App: TEMPO_URL not set or invalid: %v", err)
+		return nil, errors.New("TEMPO_URL not set or invalid")
+	}
 	username := os.Getenv("JIRA_USERNAME")
 	if username == "" {
 		log.Errorf("App: JIRA_USERNAME is not set")
@@ -43,11 +52,18 @@ func AppFromEnv() (*App, error) {
 		log.Errorf("App: JIRA_API_TOKEN is not set")
 		return nil, errors.New("JIRA_API_TOKEN is not set")
 	}
+	tempoApiToken := os.Getenv("TEMPO_API_TOKEN")
+	if tempoApiToken == "" {
+		log.Errorf("App: TEMPO_API_TOKEN is not set")
+		return nil, errors.New("TEMPO_API_TOKEN not set or invalid")
+	}
 	app := App{
-		Production: isProduction,
-		Url:        *url,
-		Username:   username,
-		ApiToken:   apiToken,
+		Production:    isProduction,
+		Url:           *url,
+		TempoUrl:      *tempoUrl,
+		Username:      username,
+		ApiToken:      apiToken,
+		TempoApiToken: tempoApiToken,
 	}
 	app.addAdapers()
 	return &app, nil
@@ -58,9 +74,11 @@ func (app *App) addAdapers() {
 		app.IssueWorklogAdapter = issue_worklog.NewWorklogJiraAdapter(app.Url, app.Username, app.ApiToken)
 		app.IssueAdapter = issue.NewJiraIssueAdapter(app.IssueWorklogAdapter, app.Url, app.Username, app.ApiToken)
 		app.FavoriteAdapter = favorite.NewFavoriteJsonAdapter("favorites.json")
+		app.TempoWorklogAdapter = tempo_worklog.NewJiraWorklogAdapter(app.TempoUrl, app.TempoApiToken)
 	} else {
 		app.IssueWorklogAdapter = issue_worklog.NewWorklogMockAdapter()
 		app.IssueAdapter = issue.NewMockIssueAdapter(app.IssueWorklogAdapter)
 		app.FavoriteAdapter = favorite.NewFavoriteJsonAdapter("favorites.json")
+		app.TempoWorklogAdapter = tempo_worklog.NewMockWorklogAdapter()
 	}
 }
