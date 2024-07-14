af6b2f4b1e843ba40a671c29115203ef794a6e20
Mathias Remshardt <remshams@gmail.com>
2024-07-14 19:13:40 +0200

    feat(tui-tempo): adds loading of timesheet status to home component

--- tui/tempo/home/home.go
+++ tui/tempo/home/home.go
@@ -2,6 +2,7 @@ package tempo_home
 
 import (
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/remshams/common/tui/bubbles/spinner"
 	"github.com/remshams/common/tui/utils"
 	jira "github.com/remshams/jira-control/jira/public"
 	tui_jira "github.com/remshams/jira-control/tui/jira"
@@ -10,17 +11,52 @@ import (
 	tempo_submit "github.com/remshams/jira-control/tui/tempo/submit"
 )
 
+type loadTimesheetStatusSuccessAction struct {
+	Status jira.TimesheetStatus
+}
+
+type loadTimesheetStatusErrorAction struct {
+	Error error
+}
+
+func (m Model) createLoadTimesheetStatusAction() tea.Cmd {
+	return func() tea.Msg {
+		statusChan := make(chan jira.TimesheetStatus)
+		errorChan := make(chan error)
+		go m.loadTimesheetStatus(statusChan, errorChan)
+		select {
+		case status := <-statusChan:
+			return loadTimesheetStatusSuccessAction{Status: status}
+		case error := <-errorChan:
+			return loadTimesheetStatusErrorAction{Error: error}
+		}
+	}
+}
+
+func (m Model) loadTimesheetStatus(statusChan chan jira.TimesheetStatus, errorChan chan error) {
+	status, err := m.timesheet.Status()
+	if err != nil {
+		errorChan <- err
+	} else {
+		statusChan <- status
+	}
+}
+
 const (
-	stateWorklog utils.ViewState = "worklog"
-	stateSubmit  utils.ViewState = "submit"
+	stateWorklog      utils.ViewState = "worklog"
+	stateSubmit       utils.ViewState = "submit"
+	stateLoading      utils.ViewState = "loading"
+	stateLoadingError utils.ViewState = "loadingError"
 )
 
 type Model struct {
-	worklogList tempo_workloglist.Model
-	submit      tempo_submit.Model
-	adapter     tui_jira.JiraAdapter
-	timesheet   jira.Timesheet
-	state       utils.ViewState
+	worklogList     tempo_workloglist.Model
+	submit          tempo_submit.Model
+	adapter         tui_jira.JiraAdapter
+	timesheet       jira.Timesheet
+	timesheetStatus jira.TimesheetStatus
+	state           utils.ViewState
+	spinner         spinner.Model
 }
 
 func New(adapter tui_jira.JiraAdapter) Model {
@@ -28,18 +64,21 @@ func New(adapter tui_jira.JiraAdapter) Model {
 		adapter:     adapter,
 		worklogList: tempo_workloglist.New(adapter),
 		submit:      tempo_submit.New(adapter),
-		state:       stateWorklog,
+		state:       stateLoading,
+		spinner:     spinner.New().WithLabel("Loading timesheet"),
 	}
 }
 
 func (m *Model) Init() tea.Cmd {
 	m.timesheet = jira.NewTimesheet(m.adapter.App.TempoTimesheetAdapter, app_store.AppDataStore.Account.AccountId)
-	return m.worklogList.Init()
+	return tea.Batch(m.createLoadTimesheetStatusAction(), m.spinner.Tick())
 }
 
 func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
 	var cmd tea.Cmd
 	switch m.state {
+	case stateLoading:
+		cmd = m.processLoadingUpdate(msg)
 	case stateWorklog:
 		cmd = m.processWorklogListUpdate(msg)
 	case stateSubmit:
@@ -48,12 +87,28 @@ func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
 	return m, cmd
 }
 
+func (m *Model) processLoadingUpdate(msg tea.Msg) tea.Cmd {
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case loadTimesheetStatusSuccessAction:
+		m.timesheetStatus = msg.Status
+		m.state = stateWorklog
+		cmd = m.worklogList.Init()
+	case loadTimesheetStatusErrorAction:
+		m.timesheetStatus = jira.TimesheetStatus{}
+		m.state = stateLoadingError
+	default:
+		m.spinner, cmd = m.spinner.Update(msg)
+	}
+	return cmd
+}
+
 func (m *Model) processWorklogListUpdate(msg tea.Msg) tea.Cmd {
 	var cmd tea.Cmd
 	switch msg.(type) {
 	case tempo_workloglist.SwitchToSubmitViewAction:
 		m.state = stateSubmit
-		cmd = m.submit.Init(m.timesheet)
+		cmd = m.submit.Init(m.timesheet, m.timesheetStatus)
 	default:
 		m.worklogList, cmd = m.worklogList.Update(msg)
 	}
@@ -74,10 +129,14 @@ func (m *Model) processSubmitUpdate(msg tea.Msg) tea.Cmd {
 
 func (m Model) View() string {
 	switch m.state {
+	case stateLoading:
+		return m.spinner.View()
 	case stateWorklog:
 		return m.worklogList.View()
 	case stateSubmit:
 		return m.submit.View()
+	case stateLoadingError:
+		return "Error loading timesheet"
 	default:
 		return "Invalid view state"
 	}
